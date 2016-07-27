63a10012ddfbd99b82e1a166234016a9b601ef8c
9min <gm0711@nate.com>
2016-07-28 00:13:52 +0900

    KanbanBoard 컴포넌트 생성

---
+++ app/KanbanBoard.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {render} from 'react-dom';
+//import List from './List';
+
+class KanbanBoard extends Component {
+    render() {
+        return (
+            <div className="app">
+                KanbanBoard
+            </div>
+        )
+    }
+}
+
+export default KanbanBoard;
