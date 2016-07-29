8ab0a9d852e3c8d1d708f6d9cdf7c31eb7e24ec6
9min <gm0711@nate.com>
2016-07-29 21:49:47 +0900

    1장까지 코딩

--- app/List.js
+++ app/List.js
@@ -2,22 +2,22 @@ import React, { Component } from 'react';
 import Card from './Card';
 
 class List extends Component {
-  render() {
-    var cards = this.props.cards.map((card) => {
-      return <Card id={card.id}
-                   title={card.title}
-                   description={card.description}
-                   tasks={card.tasks}
-                   key={card.id} />
-    });
+    render() {
+        var cards = this.props.cards.map((card) => {
+            return <Card id={card.id}
+                         title={card.title}
+                         description={card.description}
+                         tasks={card.tasks}
+                         key={card.id} />
+        });
 
-    return (
-      <div className="list">
-        <h1>{this.props.title}</h1>
-        {cards}
-      </div>
-    );
-  }
+        return (
+            <div className="list">
+                <h1>{this.props.title}</h1>
+                {cards}
+            </div>
+        );
+    }
 }
 
 export default List;
