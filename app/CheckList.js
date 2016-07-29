02b497acf2e20f420f9a0e473036fb1b7dbf3804
9min <gm0711@nate.com>
2016-07-29 22:52:05 +0900

    package.json 버전들 다 새로 받음

--- app/CheckList.js
+++ app/CheckList.js
@@ -4,8 +4,8 @@ class CheckList extends Component {
     render() {
         let tasks = this.props.tasks.map((task) => (
             <li className="checklist__task" key={task.id}>
-                <input type="checkbox" defaultChecked={task.done} />
-                {task.name}
+                <input type="checkbox" defaultChecked={task.done} id={"checkItem_"+task.id} />
+                <label htmlFor={"checkItem_"+task.id}>{task.name}</label>
                 <a href="#" className="checklist__task--remove" />
             </li>
         ));
